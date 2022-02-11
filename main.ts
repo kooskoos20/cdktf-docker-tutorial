6e0bd960a45f0d91ba2406a8df1f386c964b0eb4
kooskoos20 <omkarbirade20@gmail.com>
2022-02-11 17:42:15 +0530

    add container and image contruct to launch nginx cotainer

--- main.ts
+++ main.ts
@@ -1,11 +1,26 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
-import { DockerProvider } from "@cdktf/provider-docker";
+import { Container, DockerProvider, Image } from "@cdktf/provider-docker";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
     new DockerProvider(this, "default", {});
+    const dockerImage = new Image(this, "nginxImage", {
+      name: "nginx:latest",
+      keepLocally: false,
+    });
+
+    new Container(this, "nginxContainer", {
+      image: dockerImage.latest,
+      name: "cdktf-docker-nginx-container",
+      ports: [
+        {
+          internal: 80,
+          external: 8000,
+        },
+      ],
+    });
   }
 }
 
