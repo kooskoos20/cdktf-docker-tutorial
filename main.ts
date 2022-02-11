322745d266017d8642ee27f4abb6ba61c00c3897
kooskoos20 <omkarbirade20@gmail.com>
2022-02-11 17:37:40 +0530

    configure the docker provider in main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { DockerProvider } from "@cdktf/provider-docker";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
-
-    // define resources here
+    new DockerProvider(this, "default", {});
   }
 }
 
